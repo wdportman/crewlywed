0ac8c37931fabe2bcf1238df3fdb382e7b346560
Will <wdportman@gmail.com>
2020-11-19 22:32:24 -0800

    Attempt to set up a socket

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var bodyParser = require('body-parser');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -21,6 +20,11 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(function(req, res, next){
+  res.io = io;
+  next();
+});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
